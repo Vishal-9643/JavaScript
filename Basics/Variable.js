7173e779427947de361382c3d2013a7fe9e9a780
Vishal-9643 <golavishal82@gmail.com>
2025-09-23 07:01:05 +0530

    Declaring variable

---
+++ Basics/Variable.js
@@ -0,0 +1,15 @@
+// Const keyword used to declare constant variables i.e value of variable does not changeable.
+const studentId = 24161101;
+
+// let and var is used to declare simple variable and there values is changeable.
+let studentName = "Vishal";
+var studentEmail = "Vishal@gmail.com";
+// Prefer not to use var because of issue in block and functional scope.
+
+// studentId = 1243  // not allowed.
+
+// output printing.
+console.log(studentId);
+
+// Output printing in table form.
+console.table([studentId, studentEmail , studentName]);
