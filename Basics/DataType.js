e832ca5dbffcb36e55a560d82e32767da3b72cd7
Vishal-9643 <golavishal82@gmail.com>
2025-09-23 09:52:44 +0530

    Data Types

---
+++ Basics/DataType.js
@@ -0,0 +1,7 @@
+let name = "Vishal"; // name is string type.
+let id = 1234; // id is number type.
+let isgood = true; // isgood is boolean type.
+let crush; // crush is undefined type.
+let weakness = null; // weakness is object type.
+
+console.table([typeof name , typeof id, typeof isgood,typeof crush,typeof weakness]);
