bb15ba5230041b88baaa3955c5f46e0cdbedbe6d
Vishal-9643 <golavishal82@gmail.com>
2025-09-24 06:14:38 +0530

    Math & Number

---
+++ Basics/Number&Math.js
@@ -0,0 +1,32 @@
+const num = 400;
+console.log(num);
+
+const num2 = new Number(120.98795);
+console.log(num2);
+
+console.log(num2.toString()); // used to convert number into string.
+console.log(num2.toPrecision(3)); // used to give precise value.
+console.log(num2.toFixed(2)); // used to give given number after decimal.
+
+// MATHS :
+
+console.log(Math); // it tell that maths is a object.
+console.log(Math.abs(-56)); // used to convert -ve value into +ve.
+console.log(Math.round(4.7)); // used for round off.
+console.log(Math.max(1,7,3,8,10,12)); // used to find maximum.
+console.log(Math.min(1,9,0,-4)); // used to find minimum.
+console.log(Math.random()); // used to get random number between 0 and 1.
+
+console.log(Math.floor((Math.random()*10)+1)); // give random number between 1 to 9 .
+
+
+
+
+
+
+
+
+
+
+
+
