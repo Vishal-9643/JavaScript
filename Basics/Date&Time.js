36b0ba31daace1b6956f49ed2b8beb5abef3a3fe
Vishal-9643 <golavishal82@gmail.com>
2025-09-24 06:39:28 +0530

    Date & Time

---
+++ Basics/Date&Time.js
@@ -0,0 +1,16 @@
+let date = new Date();
+console.log(typeof date); // its type is object.
+console.log(date.toString()); // Give date in format : Day Month Month's Date time.
+console.log(date.toDateString()); // Give date in format : Day Month Month's Date.
+console.log(date.toISOString()); // Give date in format : year-month-date T01: time.
+console.log(date.toJSON()); // Give date in format : year-month-date T01: time.
+console.log(date.toLocaleDateString()); // Give date in format : date/month/year.
+console.log(date.toLocaleString()); //Give date in format : date/month/year , time .
+console.log(date.getMonth()); // here it start month from 0.
+console.log(date.getDay()); // here day start from monaday and represent as 1.
+
+
+
+
+
+
