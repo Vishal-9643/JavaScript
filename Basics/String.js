6e78cfc58611eb3c76f794aaa3e8cd29d7968846
Vishal-9643 <golavishal82@gmail.com>
2025-09-24 05:43:40 +0530

    Strings

---
+++ Basics/String.js
@@ -0,0 +1,33 @@
+const name = "Vishal"; 
+const surname = "Prajapati";
+
+console.log(name + surname); // concatation of string.
+
+console.log(`My name is ${name} ${surname}`); // Using template literals.
+
+const Game = new String('Free Fire'); // declaring string as a object.
+
+console.log(Game.length); // length function for finding length of string.
+console.log(Game.toUpperCase()); // It converts string into Upper Case.
+console.log(Game.charAt(3)); // charAt used to find character at that position.
+console.log(Game.indexOf('e')); // used to find first position of that character.
+
+const str = "I love problem solving      "
+
+console.log(str.trim()); // used to remove leading and ending whitespace.
+console.log(str.substring(2,6)); // used to print selected part with last index excluding.
+console.log(str.replace('I','We')); // used to replace word.
+console.log(str.includes('love')); // used for checking that given word is present or not.
+const str1 = "vishal-love-coding";
+console.log(str1.split('-')); // used to split string into parts.
+
+
+
+
+
+
+
+
+
+
+
